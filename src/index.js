2b5b649ff50f00dc410e8caf11eebe47bec4fac5
Tusharzade2002 <tusharzade29@gmail.com>
2024-08-25 11:22:22 +0530

    apply css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./index.css"
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from "./View/Home/Home"
+import About from "./View/About/About"
+import Contact from './View/Contact/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
 
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path: '/about',
+    element: <About/>,
+  },
+  {
+    path: '/contact',
+    element: <Contact/>,
+  },
+])
 
+root.render(<RouterProvider router={router}/>)
